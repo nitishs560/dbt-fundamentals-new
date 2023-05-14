4108f4983f5614aa04c56a104a6ad05dd8fe9459
nitishsingh560@gmail.com <nitishsingh560@gmail.com>
2023-05-14 11:00:14 +0000

    practice_exercie_in_models

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -3,7 +3,15 @@ with customers as (
 ),
 
 orders as(
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('fct_orders') }}
+),
+
+total_amount as(
+    select 
+        customer_id, 
+        sum(amount) as lifetime_value 
+    from {{ ref('fct_orders') }}
+    group by 1
 ),
 
 
@@ -14,13 +22,15 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
 
     from orders
 
     group by 1
 
 ),
+
 
 final as (
 
@@ -30,11 +40,14 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        total_amount.lifetime_value
+        
 
     from customers
 
     left join customer_orders using (customer_id)
+    inner join total_amount on customer_orders.customer_id = total_amount.customer_id
 
 )
 
